0759a989af09ea158fbc7735fb375475c7449b81
mikka <mikka@sendke.eu>
2025-07-19 20:30:19 +0200

    things do change with time

--- wezterm.lua
+++ wezterm.lua
@@ -9,6 +9,7 @@ end
 -- config.unix_domains = { { name = "unix" } }
 -- config.default_gui_startup_args = { "connect", "unix" }
 
+
 config.enable_wayland = true -- enabled by default
 config.audible_bell = "Disabled"
 config.check_for_updates = false
@@ -16,7 +17,7 @@ config.check_for_updates = false
 config.max_fps = 120 -- ignored anyway
 
 config.font = wezterm.font_with_fallback {
-    -- { family = "SF Mono",                 weight = 400 },
+    -- { family = "SF Mono Nerd Font",       weight = 400 },
     -- { family = "RobotoMonoNerdFont",      weight = 400 },
     -- { family = "Fira Mono Nerd Font", weight = 400 },
     { family = "JetBrainsMonoNLNerdFont", weight = 400 },
@@ -28,6 +29,7 @@ config.font = wezterm.font_with_fallback {
 }
 
 config.font_size = 17.3
+-- config.font_size = 14.61718
 
 config.default_prog = {
     "/run/current-system/sw/bin/zsh"
@@ -61,14 +63,16 @@ config.window_padding = {
     bottom = 0,
 }
 
--- local colorscheme = wezterm.get_builtin_color_schemes()["rose-pine"]
-local colorscheme = wezterm.get_builtin_color_schemes()["tokyonight_moon"]
+local colorscheme = wezterm.get_builtin_color_schemes()["rose-pine"]
+-- local colorscheme = wezterm.get_builtin_color_schemes()["tokyonight_moon"]
 -- local colorscheme = wezterm.get_builtin_color_schemes()["GruvboxDark"]
 
 -- colorscheme.background = "191919"
 -- colorscheme.background = "1d1d1d"
+-- colorscheme.background = "101010"
 colorscheme.background = "000000"
 -- colorscheme.background = "1f1f1f"
+
 
 colorscheme.selection_bg = "555577"
 
@@ -78,11 +82,12 @@ config.color_schemes = {
 
 config.color_scheme = "current-colorscheme"
 
-config.window_background_opacity = 0.85
+-- config.window_background_opacity = 0.65
+config.window_background_opacity = 1
 local function switch_opacity(window)
     local overrides = window:get_config_overrides() or {}
     if overrides.window_background_opacity == 1 then
-        overrides.window_background_opacity = 0.85
+        overrides.window_background_opacity = 0.65
     else
         overrides.window_background_opacity = 1
     end
@@ -343,53 +348,204 @@ config.keys = {
         action = wezterm.action.ActivateTabRelative(1),
     },
 }
+
 local sessionizer = wezterm.plugin.require "https://github.com/mikkasendke/sessionizer.wezterm"
-sessionizer.apply_to_config(config)
-local home_dir = os.getenv("HOME")
-local config_path = home_dir .. ("/.config")
-sessionizer.config.paths = {
-    home_dir .. "/dev",
-    home_dir .. "/Uni"
-}
-sessionizer.config.show_additional_before_paths = true
-sessionizer.config.additional_directories = {
-    config_path .. "/wezterm",
-    config_path .. "/nvim",
-    config_path .. "/sway",
-    config_path .. "/waybar",
-    config_path .. "/ags",
-    config_path,
-    home_dir .. "/.nixos-config",
-    home_dir .. "/dev",
+local history = wezterm.plugin.require "https://github.com/mikkasendke/sessionizer-history"
+
+
+local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
+
+table.insert(config.keys, {
+    key = "g",
+    mods = "ALT",
+    action = wezterm.action_callback(function(win, pane)
+        resurrect.state_manager.save_state(resurrect.workspace_state.get_workspace_state())
+    end),
+})
+
+local schema = {
+    options = {
+        callback =                                       -- function(window, pane, id, label)
+            history.Wrapper(sessionizer.DefaultCallback) --(window, pane, id, label)
+        -- local opts = {
+        --     relative = true,
+        --     restore_text = true,
+        --     on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+        --     close_open_tabs = true,
+        --     window = (wezterm.gui.gui_windows()[1]):mux_window(),
+        -- }
+        -- local state = resurrect.state_manager.load_state(id, "workspace")
+        -- resurrect.workspace_state.restore_workspace(state, opts)
+        -- end
+    },
+    sessionizer.DefaultWorkspace {},
+    history.MostRecentWorkspace {},
+
+    wezterm.home_dir .. "/dev",
+    wezterm.home_dir .. "/.nixos-config",
+    wezterm.home_dir .. "/.config",
+    wezterm.home_dir .. "/.config/wezterm",
+    wezterm.home_dir .. "/.config/nvim",
+    wezterm.home_dir .. "/.config/sway",
+    wezterm.home_dir .. "/.config/waybar",
+    wezterm.home_dir .. "/.config/ags",
+    wezterm.home_dir .. "/Uni",
+
+    sessionizer.FdSearch { wezterm.home_dir .. "/dev" },
+    sessionizer.FdSearch(wezterm.home_dir .. "/Uni"),
+
+    processing = sessionizer.for_each_entry(function(entry)
+        entry.label = entry.label:gsub(wezterm.home_dir, "~")
+    end)
 }
 
--- sessionizer.use_entry_processor(function(entries, next)
---     for i, entry in pairs(entries) do
---         entry.label = wezterm.format {
---             { Foreground = { Color = "Cyan" } },
---             { Text = entry.label },
---         }
---     end
---     next()
--- end)
+-- local schemaStyled = {
+--     options = { callback = history.Wrapper(sessionizer.DefaultCallback), prompt = "Choose workspace: ", title = "no one can see this" },
+--     sessionizer.DefaultWorkspace {},
+--     history.MostRecentWorkspace {},
+--     {
+--         wezterm.home_dir .. "/dev",
+--         wezterm.home_dir .. "/.nixos-config",
+--         wezterm.home_dir .. "/.config/wezterm",
+--         wezterm.home_dir .. "/.config/nvim",
+--         wezterm.home_dir .. "/.config/sway",
+--         wezterm.home_dir .. "/.config/waybar",
+--         wezterm.home_dir .. "/.config/ags",
+--         processing = sessionizer.for_each_entry(function(entry)
+--             entry.label = wezterm.format {
+--                 { Foreground = { Color = "#777777" } },
+--                 { Text = "cfg: " .. entry.label },
+--             }
+--         end)
+--     },
+--     {
+--         sessionizer.FdSearch(wezterm.home_dir .. "/dev"),
+--         processing = sessionizer.for_each_entry(function(entry)
+--             entry.label = wezterm.format {
+--                 { Foreground = { Color = "#f05133" } },
+--                 { Text = "git: " .. entry.label },
+--             }
+--         end)
+--     },
+--     {
+--         wezterm.home_dir .. "/Uni",
+--         sessionizer.FdSearch(wezterm.home_dir .. "/Uni"),
+--         processing = sessionizer.for_each_entry(function(entry)
+--             entry.label = wezterm.format {
+--                 { Foreground = { Color = "#2277dd" } },
+--                 { Text = "uni: " .. entry.label },
+--             }
+--         end)
+--     },
+--
+--     processing = sessionizer.for_each_entry(function(entry)
+--         entry.label = entry.label:gsub(wezterm.home_dir, "~")
+--     end)
+-- }
+--
+-- local schemaReplica = {
+--     options = {
+--         prompt = "Workspace to switch: ",
+--         callback = history.Wrapper(sessionizer.DefaultCallback)
+--     },
+--     {
+--         sessionizer.AllActiveWorkspaces { filter_current = false, filter_default = false },
+--         processing = sessionizer.for_each_entry(function(entry)
+--             entry.label = wezterm.format {
+--                 { Foreground = { Color = "#88cc99" } },
+--                 { Text = "󱂬 : " .. entry.label },
+--             }
+--         end)
+--     },
+--     wezterm.plugin.require "https://github.com/mikkasendke/sessionizer-zoxide.git".Zoxide {},
+--     processing = sessionizer.for_each_entry(function(entry)
+--         entry.label = entry.label:gsub(wezterm.home_dir, "~")
+--     end),
+-- }
 
--- --- @param entries { label: string, id: string }[]
--- --- @param next function
--- sessionizer.use_entry_processor(function(entries, next)
---     for _, entry in pairs(entries) do
---         entry.label = entry.label:gsub(home_dir, "~", 1)
---     end
---     next()
--- end)
----
---- @param entries { label: string, id: string }[]
---- @param next function
--- sessionizer.use_entry_processor(function(entries, next)
---     for _, entry in pairs(entries) do
---         if entry.lab
---     end
---     next()
--- end)
+local schema2 = {
+    options = {
+        prompt = "Switch branch to: ",
+        callback = function(win, pane, id, label)
+            local success, out, err = wezterm.run_child_process { "git", "-C", pane:get_current_working_dir().file_path, "switch", id }
+            -- wezterm.log_info("path: ", "(" .. pane:get_current_working_dir().file_path .. ")")
+            -- wezterm.log_info("err: ", err)
+            -- wezterm.log_info("out: ", out)
+            -- wezterm.log_info("id: ", "(" .. id .. ")")
+        end
+    },
+    function()
+        local path = ""
+        for k, v in pairs(wezterm.mux.all_windows()) do
+            if wezterm.mux.get_active_workspace() == v:get_workspace() then
+                path = v:active_pane():get_current_working_dir().file_path
+            end
+        end
+
+        local success, out, err = wezterm.run_child_process { "git", "-C", path, "branch", "-l" }
+        local s, current, e = wezterm.run_child_process { "git", "-C", path, "branch", "--show-current" }
+        current = current:gsub("\n", "")
+        local entries = {}
+
+        for line in out:gmatch "[^\n]+" do
+            line = line:gsub(" ", "")
+            line = line:gsub("*", "")
+            local entry
+
+            wezterm.log_info("line: ", "(" .. line .. ")")
+            wezterm.log_info("current: ", "(" .. current .. ")")
+            if line == current then
+                table.insert(entries, 1, {
+                    label = wezterm.format {
+                        { Foreground = { Color = "#4466ff" } },
+                        { Text = "current: " .. line },
+                    },
+                    id = line
+                })
+            else
+                entry = { label = line, id = line }
+                table.insert(entries, entry)
+            end
+        end
+        return entries
+    end
+}
+
+table.insert(config.keys, {
+    key = "s",
+    mods = "ALT",
+    action = sessionizer.show(schema)
+})
+
+table.insert(config.keys, {
+    key = "e",
+    mods = "ALT",
+    action = sessionizer.show(schema2)
+})
+--
+-- table.insert(config.keys, {
+--     key = "d",
+--     mods = "ALT",
+--     action = sessionizer.show(schemaStyled)
+-- })
+--
+-- table.insert(config.keys, {
+--     key = "g",
+--     mods = "ALT",
+--     action = sessionizer.show(schemaReplica)
+-- })
+-- local e = { options = { always_fuzzy = false, callback = schemaReplica.options.callback }, schemaReplica }
+-- table.insert(config.keys, {
+--     key = "h",
+--     mods = "ALT",
+--     action = sessionizer.show(e)
+-- })
+
+table.insert(config.keys, {
+    key = "m",
+    mods = "ALT",
+    action = history.switch_to_most_recent_workspace
+})
 
 
 return config
